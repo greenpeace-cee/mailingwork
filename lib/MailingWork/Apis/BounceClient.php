dbab2425e1849e70dd8e2bfaa7c008e6b0058441
Patrick Figel <pfigel@greenpeace.org>
2019-08-27 13:14:18 +0200

    Add getBouncesByEmailId and getRetainedEmailAddresses

--- lib/MailingWork/Apis/BounceClient.php
+++ lib/MailingWork/Apis/BounceClient.php
@@ -19,11 +19,33 @@ class BounceClient extends BaseApiClient {
     ]);
   }
 
+  public function getBouncesByEmailId($emailId, array $advanced = []) {
+    return $this->client->request('getBouncesByEmailId', [
+      'emailId' => $emailId,
+      'advanced' => $advanced,
+    ]);
+  }
+
+  public function getRetainedEmailAddresses(array $advanced = []) {
+    return $this->client->request('getRetainedEmailAddresses', [
+      'advanced' => $advanced,
+    ]);
+  }
+
   public static function getErrorCodes() {
     return [
       'getBounces' => [
         2 => 'invalid field to filter',
         3 => 'invalid emailId',
+      ],
+      'getBouncesByEmailId' => [
+        2 => 'invalid field to filter',
+        3 => 'invalid emailId',
+      ],
+      'getRetainedEmailAddresses' => [
+        2 => 'invalid lastHardBounceFrom',
+        3 => 'invalid lastHardBounceTo',
+        4 => 'lastHardBounceFrom bigger than lastHardBounceTo',
       ],
     ];
   }
